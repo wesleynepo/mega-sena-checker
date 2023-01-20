25a7dc713ff20875447fc30a25622328c8c0062e
wesleynepo <wesleynepomuceno1@gmail.com>
2023-01-19 23:47:38 -0300

    feat: google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-EN7MKWXBLG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-EN7MKWXBLG');
+        `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
